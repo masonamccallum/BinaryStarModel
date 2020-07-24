eeb5ca7572263ce1e95c49b544eaa189eabca212
masonamccallum <masonamccallum@gmail.com>
2020-07-24 16:45:11 -0500

    imgui fix

--- source/src/application.cpp
+++ source/src/application.cpp
@@ -1,4 +1,3 @@
-//g++ testTwo.cpp -o tTwo -lglfw -lGL -lGLEW
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -77,18 +76,74 @@ int main(void){
 
 		Renderer renderer;
 
-    while(!glfwWindowShouldClose(window)){
-			renderer.Clear();
-			shader.Bind();
-			shader.SetUniform4f("u_color", 0.8f,0.3f,0.8f,1.0f);
+		ImGui::CreateContext();
+		ImGui::StyleColorsDark();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		const char* glsl_version = "#version 330";
+		ImGui_ImplOpenGL3_Init(glsl_version);
 
-			renderer.Draw(va,ib,shader);
-
-      glfwSwapBuffers(window);
-			glfwPollEvents();
-    }
 		
-		glfwDestroyWindow(window);
+
+		bool show_demo_window = true;
+    	bool show_another_window = false;
+    	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while(!glfwWindowShouldClose(window)){
+		renderer.Clear();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		shader.Bind();
+		shader.SetUniform4f("u_color", 0.8f,0.3f,0.8f,1.0f);
+		renderer.Draw(va,ib,shader);
+
+		if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		
+
+      	glfwSwapBuffers(window);
+		glfwPollEvents();
+    }
+	
+	ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+	glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
